c8da349eb0964326dc530f7fde54da0ddd3ee897
Rose Cota <rosejcota@gmail.com>
2022-05-06 15:58:14 -0500

    add main.css to index.html

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,18 +1,30 @@
-import { Facilities } from "./Facilities.js"
-import { Governors } from "./Governor.js"
+import { Facilities } from "./Facilities.js";
+import { Governors } from "./Governor.js";
 
 export const Exomine = () => {
   return `
         <h1>Solar System Mining Marketplace</h1>
-        ;
-        <article class="choices">
+        
+        <div class="choices">
             <section class="governorChoices">
                 
                 ${Governors()}
             </section>
-            <article class="choices">
+        </div>
+        <div class="choices">
             <section class="facilityChoices">
                 
                 ${Facilities()}
-            </section>`
+            </section>
+        </div>
+        
+        <div class="flex-grid">
+            <div class="col">
+               <h2>Minerals at Facility</h2>
+            </div>
+            <div class="col">
+               <h4>Cart</h4>
+            </div>
+        </div>
+            `;
 };
